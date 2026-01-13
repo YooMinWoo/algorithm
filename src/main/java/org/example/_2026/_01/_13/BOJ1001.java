2bba00c6811d94499996de4c4b2535a2d7d331dd
YooMinWoo <alsn0527@naver.com>
2026-01-13 21:17:50 +0900

    BOJ1001

---
+++ src/main/java/org/example/_2026/_01/_13/BOJ1001.java
@@ -0,0 +1,14 @@
+package org.example._2026._01._13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1001 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+
+        System.out.println(Integer.parseInt(str[0]) - Integer.parseInt(str[1]));
+    }
+}
