c309d7e476d7e2e5df8b505d18624014e9adca66
YooMinWoo <alsn0527@naver.com>
2026-01-13 21:00:37 +0900

    BOJ2557

---
+++ src/main/java/org/example/_2026/_01/_13/BOJ2557.java
@@ -0,0 +1,11 @@
+package org.example._2026._01._13;
+
+/**
+ * Hello World
+ * 문제 : Hello World!를 출력하시오.
+ */
+public class BOJ2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
