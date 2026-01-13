bdaa07e76d179a9d90b1c4819c1c640729162958
YooMinWoo <alsn0527@naver.com>
2026-01-13 22:50:21 +0900

    BOJ10430

---
+++ src/main/java/org/example/_2026/_01/_13/BOJ10430.java
@@ -0,0 +1,29 @@
+package org.example._2026._01._13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 나머지
+ * 
+ * (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+ * (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+ * 세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+ */
+public class BOJ10430 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+        int C = Integer.parseInt(input[2]);
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C)*(B%C))%C);
+    }
+}
