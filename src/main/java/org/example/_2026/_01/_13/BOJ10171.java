e0b022f8eb824fe77575e08ea911648fe97c4254
YooMinWoo <alsn0527@naver.com>
2026-01-13 23:19:55 +0900

    BOJ10171

---
+++ src/main/java/org/example/_2026/_01/_13/BOJ10171.java
@@ -0,0 +1,14 @@
+package org.example._2026._01._13;
+
+/**
+ * 고양이
+ */
+public class BOJ10171 {
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
