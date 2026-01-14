1c1684d0dee1b74e250ed363a59b4d5b5cc953dc
YooMinWoo <alsn0527@naver.com>
2026-01-15 05:34:28 +0900

    BOJ14720_v2

--- src/main/java/org/example/_2026/_01/_15/BOJ14720.java
+++ src/main/java/org/example/_2026/_01/_15/BOJ14720.java
@@ -3,30 +3,49 @@ package org.example._2026._01._15;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BOJ14720 {
     public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int N = Integer.parseInt(br.readLine());
+//        String[] s = br.readLine().split(" ");
+//
+//        int curr = 2;
+//        int cnt = 0;
+//        for(String str : s){
+//            int i = Integer.parseInt(str);
+//            if(curr == 2 && i == 0) {
+//                cnt++;
+//                curr = i;
+//            }
+//            else if (curr == 0 && i == 1) {
+//                cnt++;
+//                curr = i;
+//            }
+//            else if (curr == 1 && i == 2) {
+//                cnt++;
+//                curr = i;
+//            }
+//        }
+//        System.out.println(cnt);
+
+        // 방법 2
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        String[] s = br.readLine().split(" ");
 
-        int curr = 2;
-        int cnt = 0;
-        for(String str : s){
-            int i = Integer.parseInt(str);
-            if(curr == 2 && i == 0) {
-                cnt++;
-                curr = i;
-            }
-            else if (curr == 0 && i == 1) {
-                cnt++;
-                curr = i;
-            }
-            else if (curr == 1 && i == 2) {
-                cnt++;
-                curr = i;
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int curr = 0;
+        int answer = 0;
+
+        for(int i=0; i<N; i++){
+            int n = Integer.parseInt(st.nextToken());
+            if(curr == n){
+                curr = curr == 2 ? 0 : curr+1;
+                answer++;
             }
         }
-        System.out.println(cnt);
+        System.out.println(answer);
     }
 }
