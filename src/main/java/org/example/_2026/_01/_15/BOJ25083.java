2e04bbac7a85d142c67dffb05445bbaf918b2724
YooMinWoo <alsn0527@naver.com>
2026-01-15 05:00:59 +0900

    BOJ25083

---
+++ src/main/java/org/example/_2026/_01/_15/BOJ25083.java
@@ -0,0 +1,12 @@
+package org.example._2026._01._15;
+
+public class BOJ25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
