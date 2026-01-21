d4c55e2e46dde64c3a63a9f7caba3103787964fb
YooMinWoo <alsn0527@naver.com>
2026-01-21 21:13:10 +0900

    BOJ2739

---
+++ src/main/java/org/example/_2026/_01/_21/BOJ2739.java
@@ -0,0 +1,16 @@
+package org.example._2026._01._21;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2739 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=9; i++){
+            System.out.println(N + " * " + i + " = " + (N*i));
+        }
+    }
+}
