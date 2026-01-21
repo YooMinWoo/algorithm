47b563020df5c440608dc8d55af682a778ac9f0a
YooMinWoo <alsn0527@naver.com>
2026-01-21 23:49:09 +0900

    BOJ2438

---
+++ src/main/java/org/example/_2026/_01/_21/BOJ2438.java
@@ -0,0 +1,21 @@
+package org.example._2026._01._21;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=T; i++){
+            for(int j=0; j<i; j++){
+                sb.append("*");
+            }
+            if(i < T) sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
