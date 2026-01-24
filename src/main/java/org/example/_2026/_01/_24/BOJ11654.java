10dadf5f4595b4f189dbe7559727cee526f3df1f
YooMinWoo <alsn0527@naver.com>
2026-01-24 16:26:25 +0900

    BOJ11654

---
+++ src/main/java/org/example/_2026/_01/_24/BOJ11654.java
@@ -0,0 +1,13 @@
+package org.example._2026._01._24;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] charArray = br.readLine().toCharArray();
+        System.out.println((int) charArray[0]);
+    }
+}
