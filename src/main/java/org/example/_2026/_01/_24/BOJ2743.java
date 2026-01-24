790631da1528de61a203667590a10b398cc74f03
YooMinWoo <alsn0527@naver.com>
2026-01-24 16:15:43 +0900

    BOJ2743

---
+++ src/main/java/org/example/_2026/_01/_24/BOJ2743.java
@@ -0,0 +1,13 @@
+package org.example._2026._01._24;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        System.out.println(S.length());
+    }
+}
