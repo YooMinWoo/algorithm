675aca93fe7614353a83fa94f76118b3b3ba7132
YooMinWoo <alsn0527@naver.com>
2026-01-26 21:04:28 +0900

    BOJ11718

---
+++ src/main/java/org/example/_2026/_01/_26/BOJ11718.java
@@ -0,0 +1,16 @@
+package org.example._2026._01._26;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true){
+            String s = br.readLine();
+            if(s == null) break;
+            System.out.println(s);
+        }
+    }
+}
