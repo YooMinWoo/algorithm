320bb49313c9fd0b3db5defadbc0615101aa2579
YooMinWoo <alsn0527@naver.com>
2026-01-30 01:37:00 +0900

    BOJ10101

---
+++ src/main/java/org/example/_2026/_01/_30/BOJ10101.java
@@ -0,0 +1,18 @@
+package org.example._2026._01._30;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10101 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        if(a==60 && b==60 && c==60) System.out.println("Equilateral");
+        else if (a+b+c == 180 && (a==b || b==c || a==c)) System.out.println("Isosceles");
+        else if (a+b+c == 180 && (a!=b && a!=c && b!=c)) System.out.println("Scalene");
+        else System.out.println("Error");
+    }
+}
