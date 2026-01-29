28b02f6f9d50570f44b1cd0bc6f6207147af1ec4
YooMinWoo <alsn0527@naver.com>
2026-01-30 03:42:47 +0900

    BOJ24262

---
+++ src/main/java/org/example/_2026/_01/_30/BOJ24262.java
@@ -0,0 +1,9 @@
+package org.example._2026._01._30;
+
+
+public class BOJ24262 {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("0");
+    }
+}
