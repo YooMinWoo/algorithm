3f5227e754f8d17d9c629c34c3d6480a551acc7b
YooMinWoo <alsn0527@naver.com>
2026-01-30 03:49:19 +0900

    BOJ24264

---
+++ src/main/java/org/example/_2026/_01/_30/BOJ24264.java
@@ -0,0 +1,12 @@
+package org.example._2026._01._30;
+
+import java.util.Scanner;
+
+public class BOJ24264 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long N = sc.nextLong();
+        System.out.println(N*N);
+        System.out.println("2");
+    }
+}
