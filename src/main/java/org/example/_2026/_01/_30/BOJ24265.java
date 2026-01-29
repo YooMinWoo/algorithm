fc63bdeb296d181ace257a636a8bcaf84514dd6d
YooMinWoo <alsn0527@naver.com>
2026-01-30 04:06:13 +0900

    BOJ24265

---
+++ src/main/java/org/example/_2026/_01/_30/BOJ24265.java
@@ -0,0 +1,14 @@
+package org.example._2026._01._30;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ24265 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        System.out.println(n*n - (n*(n+1)/2));
+        System.out.println("2");
+    }
+}
